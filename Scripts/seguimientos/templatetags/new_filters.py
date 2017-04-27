8c6656e508ce88f7336f87694f5461f2b5a8df30
Alejandro Demergassi <ademergassi@univisiononline.com.ar>
2017-04-27 12:32:16 -0300

    - Stats -
    Fixes: Correcto cálculo de los días / meses / años.
    
    - Templates -
    
    Primera versión del footer.

--- Scripts/seguimientos/templatetags/new_filters.py
+++ Scripts/seguimientos/templatetags/new_filters.py
@@ -18,7 +18,8 @@ def points_sum(user):
     """Suma los puntos del vendedor para el mes en curso."""
     mydate = datetime.datetime.now()
     current_month = mydate.strftime( "%m" )
-    ventas = VentaNueva.objects.filter( owner=user.id, payoff_date__month=current_month )
+    current_year = mydate.strftime( "%Y" )
+    ventas = VentaNueva.objects.filter( owner=user.id, payoff_date__month=current_month, date_added__year=current_year )
     i = 0
     for venta in ventas:
         if venta.payoff == True and venta.status in ('Finalizada', 'OK'):
@@ -31,7 +32,8 @@ def monthly_sales(user):
     """Cuenta la cantidad total de ventas en el mes actual."""
     mydate = datetime.datetime.now()
     current_month = mydate.strftime( "%m" )
-    ventas = VentaNueva.objects.filter( date_added__month=current_month )
+    current_year = mydate.strftime( "%Y" )
+    ventas = VentaNueva.objects.filter( date_added__month=current_month, date_added__year=current_year )
     i = 0
     for venta in ventas:
         i += 1
@@ -43,9 +45,11 @@ def last_month_sales(user):
     """Cuenta la cantidad total de ventas en el mes anterior."""
     mydate = datetime.datetime.now()
     current_month = mydate.strftime( "%m" )
+    current_year = mydate.strftime( "%Y" )
     if current_month == '01':
         current_month = '13'
-    ventas = VentaNueva.objects.filter( date_added__month=int(current_month) -1 )
+        current_year = int(current_year) - 1
+    ventas = VentaNueva.objects.filter( date_added__month=int(current_month) -1, date_added__year=current_year )
     i = 0
     for venta in ventas:
         i += 1
@@ -57,9 +61,12 @@ def total_daily_sales(user):
     """Cuenta cantidad de ventas diarias POR GRUPO."""
     mydate = datetime.datetime.now()
     current_day = mydate.strftime( "%d" )
+    current_month = mydate.strftime( "%m" )
+    current_year = mydate.strftime( "%Y" )
     sale_locations = {"IRS0400": 0, "IRS0401": 0, "IRS0402": 0, "IRS0403": 0, "IRS0405": 0, "IRS0406": 0}
     for loc in sale_locations.keys():
-        ventas = VentaNueva.objects.filter( date_added__day=current_day, owner__groups__name=str(loc) )
+        ventas = VentaNueva.objects.filter( date_added__day=current_day, date_added__month=current_month,
+                                                date_added__year=current_year, owner__groups__name=str(loc) )
         i = 0
         for venta in ventas:
             i += 1
@@ -80,6 +87,8 @@ def total_daily_sales_by_vendor(user):
     """Cuenta cantidad de ventas diarias POR VENDEDOR."""
     mydate = datetime.datetime.now()
     current_day = mydate.strftime( "%d" )
+    current_month = mydate.strftime( "%m" )
+    current_year = mydate.strftime( "%Y" )
     vendor_name = []
     vendor_name.append( ["Vendedor" , "Cantidad"] )
     users = User.objects.all().exclude(groups__name="Liquidaciones")
@@ -88,7 +97,8 @@ def total_daily_sales_by_vendor(user):
 
     for v in range(len(vendor_name)):
         for vendor in vendor_name[v]:
-            ventas = VentaNueva.objects.filter( date_added__day=current_day , owner__username=vendor )
+            ventas = VentaNueva.objects.filter( date_added__day=current_day, date_added__month=current_month,
+                                                date_added__year=current_year, owner__username=vendor )
             i = 0
             for venta in ventas:
                 i += 1
@@ -111,7 +121,8 @@ def total_daily_sales_per_month(user):
 
     for d in range(len(days_list)):
         for day in days_list[d]:
-            ventas = VentaNueva.objects.filter( date_added__day=str(d) )
+            ventas = VentaNueva.objects.filter( date_added__day=str(d), date_added__month=current_month,
+                                                date_added__year=current_year)
             i = 0
             for venta in ventas:
                 i += 1
